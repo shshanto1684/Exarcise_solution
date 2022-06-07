a8f941a2b61a360ef9b97bfce5bc2ba04d5362bf
shshanto1684 <sahadathossainshanto1684@gmail.com>
2022-06-07 17:32:43 +0600

    this file modified condition

--- E_4.1/01.c
+++ E_4.1/01.c
@@ -2,21 +2,21 @@
 
 int main()
 {
-    int numbers[ ] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int numbers[10];
     int i, j, tem;
 
-    /*for(i = 0; i < 10; i++) {
+    for(i = 0; i < 10; i++) {
         scanf("%d", &numbers[i]);
-    }*/
+    }
 
-        for(i = 0; i < 5; i++) {
+        for(i = 0, j = 9; i < 10 && j > i; j--, i++) {
         tem = numbers[j];
         numbers[j] = numbers[i];
         numbers[i] = tem;
     }
 
-    for(j = 9; j >= 0; j--) {
-        printf("%d ", numbers[j]);
+    for(i = 0; i < 10; i++) {
+        printf("%d ", numbers[i]);
     }
 
     return 0;
